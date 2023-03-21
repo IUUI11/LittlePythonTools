58fc7911569f257cea170752969206b2002c13d8
likeafool <41223176+IUUI11@users.noreply.github.com>
2023-03-21 15:39:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -37,7 +37,7 @@ import torch.nn as nn
 # pic1_transf = transf(pic1)
 # # print(pic1_transf.shape)
 # det_out, da_seg_out,ll_seg_out = yolo(pic1_transf)
-
+# 递归查找文件夹中的文件
 import shutil
 import os
 testpath = "F:\postgraduate\数据集 街景天气\Copy of rainy1\\rainy"
@@ -53,4 +53,24 @@ for root, dirs, files in os.walk(testpath):
         print(src)
         shutil.copy2(src,targetpath)
         l = l - 1
+# 将某个文件夹中 文件名中带有关键字 的文件 复制到另一个文件夹
+import os
 
+src_dir_path = 'F:\\postgraduate\\数据集 街景天气\\sunnytest'  # 源文件夹
+
+to_dir_path = 'F:\\postgraduate\\数据集 街景天气\\'  # 存放复制文件的文件夹
+
+key = '3_00013'  # 源文件夹中的文件包含字符key则复制到to_dir_path文件夹中
+
+if not os.path.exists(to_dir_path):
+    print("to_dir_path not exist,so create the dir")
+    os.mkdir(to_dir_path, 1)
+if os.path.exists(src_dir_path):
+    print("src_dir_path exist")
+    for file in os.listdir(src_dir_path):
+        # is file
+        if os.path.isfile(src_dir_path + '/' + file):
+            if key in file:
+                print('找到包含"' + key + '"字符的文件,绝对路径为----->' + src_dir_path + '/' + file)
+                print('复制到----->' + to_dir_path + file)
+                shutil.copy(src_dir_path + '/' + file, to_dir_path + '/' + file)  # 移动用move函数
